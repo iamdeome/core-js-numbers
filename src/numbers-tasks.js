9e7deadc2e0a068514043b6eb3f38026223c3435
John Doe <2ivShr2!>
2024-10-28 21:06:51 +0100

    feat: implement isPrime

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -178,7 +178,10 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
+function roundToPowerOfTen(num, pow) {
+  let a = num + pow;
+  const b = a;
+  a += b;
   throw new Error('Not implemented');
 }
 
@@ -199,8 +202,11 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 2; i <= n ** (1 / 2); i += 1) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
 
 /**
